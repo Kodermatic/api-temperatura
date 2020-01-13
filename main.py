917a0c2c2a20ff4daf7ce0962bd4fcfd4ea2dcf5
Kodermatic <56317944+Kodermatic@users.noreply.github.com>
2020-01-13 18:13:37 +0100

    init

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import requests
+from random import randint
+
+app = Flask(__name__)
+
+api_key = open("api_key.txt").read().strip()
+# url = f"https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID={api_key}"
+
+@app.route("/", methods=["GET"])
+def index_get():
+  ime_mesta = ["Ljubljana", "London", "Paris"][randint(0,2)]
+  url = f"https://api.openweathermap.org/data/2.5/weather?q={ime_mesta}&units=metric&APPID={api_key}"
+  result = requests.get(url).json()
+  return render_template("index.html", data = result)
+
+if __name__ == "__main__":
+  app.run()
+  
